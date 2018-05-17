74a7741a04067c7183ec186ac3e17548474ac7ee
Arpan Parikh <f2016082@pilani.bits-pilani.ac.in>
2018-05-17 10:37:04 +0530

    Update main.go

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ type Message struct {
 	Message  string `json:"message"`
 }
 func main() {
-	fs := http.FileServer(http.Dir("../public"))
+	fs := http.FileServer(http.Dir("../"))
 	http.Handle("/", fs)
 	http.HandleFunc("/ws", handleConnections)
 	go handleMessages()
